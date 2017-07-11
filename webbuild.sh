a860cd9f5c9a5c69ed621555ad4c289b89409c49
Bigondis <lee.abner@cibmis.com>
2017-07-11 14:40:44 -0500

    Intial Commit

--- webbuild.sh
+++ webbuild.sh
@@ -10,20 +10,28 @@ sudo a2enmod ssl
 sudo a2enmod include
 
 #Setup Directors for websites
-sudo mkdir -p /data/www/avebank.com
-sudo mkdir -p /data/www/marinebank.com
-sudo mkdir -p /data/www/centralillinoisbank.com
-sudo mkdir -p /data/www/cibmbank.com
+sudo mkdir -p /var/www/avebank.com
+sudo mkdir -p /var/www/marinebank.com
+sudo mkdir -p /var/www/centralillinoisbank.com
+sudo mkdir -p /var/www/cibmbank.com
 
 #Setup appropriate rights to folders
 sudo chmod -R 755 /data/www
 
 #Clone websites from Github
-sudo git clone https://github.com/cibmis/cib.git /data/www/centralillinoisbank.com
-sudo git clone https://github.com/cibmis/ave.git /data/www/avebank.com
-sudo git clone https://github.com/cibmis/marine.git /data/www/marinebank.com
-sudo git clone https://github.com/cibmis/cibm.git /data/www/cibmbank.com
+sudo git clone https://github.com/cibmis/cib.git /var/www/centralillinoisbank.com
+sudo git clone https://github.com/cibmis/ave.git /var/www/avebank.com
+sudo git clone https://github.com/cibmis/marine.git /var/www/marinebank.com
+sudo git clone https://github.com/cibmis/cibm.git /var/www/cibmbank.com
 
-sudo chmod +x /data/www/centralillinoisbank.com/*.html
-sudo chmod +x /data/www/avebank.com/*.html
-sudo chmod +x /data/www/marinebank.com/*.html
+sudo chmod +x /var/www/centralillinoisbank.com/*.html
+sudo chmod +x /var/www/avebank.com/*.html
+sudo chmod +x /var/www/marinebank.com/*.html
+
+sudo git clone https://github.com/cibmis/wwwconfig ~/config
+sudo mkdir /etc/apache2/ssl
+sudo cp ~/config/* /etc/apache2/ssl/.
+sudo cp ~/config/*bank.conf /etc/apache2/sites-available/.
+sudo a2ensite avebank.conf
+sudo a2ensite centralillinoisbank.conf
+sudo a2ensite marinebank.conf
